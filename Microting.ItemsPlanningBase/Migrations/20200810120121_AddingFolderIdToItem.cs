d156bf246b812e1f918af841dacfcf882da9fd8b
René Schultz Madsen <rm@microting.com>
2020-08-10 13:37:38 +0000

    Adding the new folder part.

---
+++ Microting.ItemsPlanningBase/Migrations/20200810120121_AddingFolderIdToItem.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Microting.ItemsPlanningBase.Migrations
+{
+    public partial class AddingFolderIdToItem : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "eFormSdkFolderId",
+                table: "ItemVersions",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "eFormSdkFolderId",
+                table: "Items",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "eFormSdkFolderId",
+                table: "ItemVersions");
+
+            migrationBuilder.DropColumn(
+                name: "eFormSdkFolderId",
+                table: "Items");
+        }
+    }
+}
