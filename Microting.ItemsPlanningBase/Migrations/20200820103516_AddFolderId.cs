011477b61a2fd6c83746f2b822b674f0f129a8e9
Leonid <leonid.atf@gmail.com>
2020-08-20 13:36:14 +0300

    Add folder id migration

---
+++ Microting.ItemsPlanningBase/Migrations/20200820103516_AddFolderId.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Microting.ItemsPlanningBase.Migrations
+{
+    public partial class AddFolderId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "FolderId",
+                table: "PlanningVersions",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "FolderId",
+                table: "Plannings",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FolderId",
+                table: "PlanningVersions");
+
+            migrationBuilder.DropColumn(
+                name: "FolderId",
+                table: "Plannings");
+        }
+    }
+}
